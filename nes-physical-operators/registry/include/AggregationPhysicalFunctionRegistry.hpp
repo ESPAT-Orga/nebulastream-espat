615f1101365f0a0883b3a783796f04136353f125
Nils L. Schubert <nilslpschubert@gmail.com>
2025-09-11 11:28:39 +0200

    feat(Statistic Build): Added a reservoir sample build operator

--- nes-physical-operators/registry/include/AggregationPhysicalFunctionRegistry.hpp
+++ nes-physical-operators/registry/include/AggregationPhysicalFunctionRegistry.hpp
@@ -35,7 +35,29 @@ struct AggregationPhysicalFunctionRegistryArguments
     DataType resultType;
     PhysicalFunction inputFunction;
     Record::RecordFieldIdentifier resultFieldIdentifier;
-    std::optional<std::shared_ptr<Interface::MemoryProvider::TupleBufferMemoryProvider>> memProviderPagedVector;
+    std::shared_ptr<Interface::MemoryProvider::TupleBufferMemoryProvider> memProviderPagedVector;
+    std::optional<std::string> numberOfSeenTuplesFieldName{};
+    std::optional<uint64_t> seed{};
+    std::optional<uint64_t> sampleSize{};
+    std::optional<uint64_t> numberOfBins{};
+    std::optional<uint64_t> minValue{};
+    std::optional<uint64_t> maxValue{};
+    std::optional<uint64_t> columns{};
+    std::optional<uint64_t> rows{};
+
+    AggregationPhysicalFunctionRegistryArguments(
+        const DataType inputType,
+        const DataType resultType,
+        PhysicalFunction inputFunction,
+        Record::RecordFieldIdentifier resultFieldIdentifier,
+        std::shared_ptr<Interface::MemoryProvider::TupleBufferMemoryProvider> memProviderPagedVector)
+        : inputType(inputType)
+        , resultType(resultType)
+        , inputFunction(std::move(inputFunction))
+        , resultFieldIdentifier(std::move(resultFieldIdentifier))
+        , memProviderPagedVector(std::move(memProviderPagedVector))
+    {
+    }
 };
 
 class AggregationPhysicalFunctionRegistry : public BaseRegistry<

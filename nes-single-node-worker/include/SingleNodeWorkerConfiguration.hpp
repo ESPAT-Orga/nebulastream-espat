3600557930e883fe9bdcb5b7d50df0e62fa803b3
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-11-20 18:31:10 +0100

    distributed
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

--- nes-single-node-worker/include/SingleNodeWorkerConfiguration.hpp
+++ nes-single-node-worker/include/SingleNodeWorkerConfiguration.hpp
@@ -24,7 +24,7 @@ namespace NES
 class SingleNodeWorkerConfiguration final : public BaseConfiguration
 {
 public:
-    StringOption connection = {"connection", "", "Connection name. This is the {Hostname}:{PORT}"};
+    ScalarOption<NES::URI> connection = {"connection", "Connection name. This is the {Hostname}:{PORT}"};
 
     /// GRPC Server Address URI. By default, it binds to any address and listens on port 8080
     StringOption grpcAddressUri

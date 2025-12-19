0cb87aaba1e4b73a30d0b021eb1e46f48f5161b7
Nils L. Schubert <nilslpschubert@gmail.com>
2025-12-19 16:33:29 +0100

    feat(BM-Listener): Got rid of the originId

--- nes-query-engine/tests/Util/QueryEngineTestingInfrastructure.cpp
+++ nes-query-engine/tests/Util/QueryEngineTestingInfrastructure.cpp
@@ -238,7 +238,11 @@ QueryPlanBuilder::TestPlanCtrl QueryPlanBuilder::build(QueryId queryId, std::sha
     {
         std::vector<std::weak_ptr<ExecutablePipeline>> successors;
         std::ranges::transform(forwardRelations.at(source.first), std::back_inserter(successors), getOrCreatePipeline);
-        auto [s, ctrl] = getTestSource(backpressureListener, std::get<SourceDescriptor>(source.second).sourceId, bm);
+        auto [s, ctrl] = getTestSource(
+            backpressureListener,
+            std::get<SourceDescriptor>(source.second).sourceId,
+            std::get<SourceDescriptor>(source.second).pipelineId,
+            bm);
         sourceIds.emplace(source.first, s->getSourceId());
         sources.emplace_back(std::move(s), std::move(successors));
         sourceCtrls[source.first] = ctrl;
@@ -309,36 +313,39 @@ void TestingHarness::expectQueryStatusEvents(QueryId id, std::initializer_list<Q
                 queryRunning.emplace(id, std::make_unique<std::promise<void>>());
                 EXPECT_CALL(*status, logQueryStatusChange(id, QueryState::Running, ::testing::_))
                     .Times(1)
-                    .WillOnce(::testing::Invoke(
-                        [this](auto id, auto, auto)
-                        {
-                            queryRunning.at(id)->set_value();
-                            return true;
-                        }));
+                    .WillOnce(
+                        ::testing::Invoke(
+                            [this](auto id, auto, auto)
+                            {
+                                queryRunning.at(id)->set_value();
+                                return true;
+                            }));
                 break;
             case QueryState::Stopped:
                 ASSERT_TRUE(queryTermination.try_emplace(id, std::make_unique<std::promise<void>>()).second)
                     << "Registered multiple query terminations";
                 EXPECT_CALL(*status, logQueryStatusChange(id, QueryState::Stopped, ::testing::_))
                     .Times(1)
-                    .WillOnce(::testing::Invoke(
-                        [this](auto id, auto, auto)
-                        {
-                            queryTermination.at(id)->set_value();
-                            return true;
-                        }));
+                    .WillOnce(
+                        ::testing::Invoke(
+                            [this](auto id, auto, auto)
+                            {
+                                queryTermination.at(id)->set_value();
+                                return true;
+                            }));
                 break;
             case QueryState::Failed:
                 ASSERT_TRUE(queryTermination.try_emplace(id, std::make_unique<std::promise<void>>()).second)
                     << "Registered multiple query terminations";
                 EXPECT_CALL(*status, logQueryFailure(id, ::testing::_, ::testing::_))
                     .Times(1)
-                    .WillOnce(::testing::Invoke(
-                        [this](const auto& id, const auto&, auto)
-                        {
-                            queryTermination.at(id)->set_value();
-                            return true;
-                        }));
+                    .WillOnce(
+                        ::testing::Invoke(
+                            [this](const auto& id, const auto&, auto)
+                            {
+                                queryTermination.at(id)->set_value();
+                                return true;
+                            }));
                 break;
         }
     }

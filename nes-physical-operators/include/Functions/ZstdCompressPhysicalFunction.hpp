ad6849d3e4561f8b0dfb60098d20c4d03fc45464
Tilman <nmlt@users.noreply.github.com>
2026-03-05 17:51:13 +0100

    fixup! Fix(Zstd): Adapt to new serialization with reflection

--- nes-physical-operators/include/Functions/ZstdCompressPhysicalFunction.hpp
+++ nes-physical-operators/include/Functions/ZstdCompressPhysicalFunction.hpp
@@ -22,18 +22,21 @@
 namespace NES
 {
 
-class ZstdCompressPhysicalFunction final : public PhysicalFunctionConcept
+class ZstdCompressPhysicalFunction final
 {
 public:
     ZstdCompressPhysicalFunction(PhysicalFunction childPhysicalFunction, DataType type, uint32_t compressionLevel);
     size_t static compress(
         size_t inputSize, int8_t* inputData, size_t compressedMaxSize, int8_t* compressedData, uint32_t compressionLevel);
     static void copyCompressionResultAndSize(int8_t* destination, int8_t* source, size_t compressedSize);
-    [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const override;
+    [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const;
 
 private:
     PhysicalFunction childPhysicalFunction;
     DataType type;
     uint32_t compressionLevel;
 };
+
+static_assert(PhysicalFunctionConcept<ZstdCompressPhysicalFunction>);
+
 }

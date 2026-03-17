c54767195f98e491ae3fc460933a82865e523368
fkuzun <f.lang@tu-berlin.de>
2026-03-17 10:35:08 +0100

    implement benchmarking script, add notebook

--- scripts/benchmarking/multiple-statistic-queries-over-time/run_multiple_statistic_queries_over_time_variable.sh
+++ scripts/benchmarking/multiple-statistic-queries-over-time/run_multiple_statistic_queries_over_time_variable.sh
@@ -19,7 +19,7 @@ python3 -m venv myenv
 source myenv/bin/activate
 pip install argparse requests pandas pyyaml
 
-myenv/bin/python3 -m scripts.benchmarking.work-dealing.run_nes_work_dealing_stealing --wait-between-queries 3 --wait-before-stopping-queries 5 --generator-rates scripts/benchmarking/work-dealing/work-dealing-fixed/ingestion_queries_fixed.yaml --number-of-queries 4 --buffer-size 8192 --number-of-buffers 4000000 --flush-interval 100
+myenv/bin/python3 -m scripts.benchmarking.multiple-statistic-queries-over-time.run_nes_multiple_statistic_queries_over_time --wait-between-queries 3 --wait-before-stopping-queries 30 --generator-rates scripts/benchmarking/multiple-statistic-queries-over-time/ingestion-variable/ingestion_queries_variable.yaml --number-of-queries 2 --statistics-generator-rates scripts/benchmarking/multiple-statistic-queries-over-time/ingestion-variable/ingestion_queries_variable.yaml --number-of-statistics-query-starts 10 --statistics-queries-per-start 2 --buffer-size 10240 --number-of-buffers 200000 --flush-interval 5
 
 # Deactivate the virtual environment
 deactivate

nebulastream/nebulastream

feat(QueryId): Introduce UUID-based QueryId
Replace integer-based query identifiers with UUID-based LocalQueryId wrapped
in a QueryId class. QueryId uses static factory methods (createLocal, invalid).

- Add LocalQueryId as NESStrongUUIDType with UUID generation
- Add QueryId wrapper class. Allows future extension with disitributed ids
- Add SerializableQueryId protobuf message for gRPC serialization
- Update LogicalPlan to require explicit QueryId for construction

nes-nebuli/src/Statements/StatementHandler.cpp
@@ -230,7 +230,7 @@ std::expected<QueryStatementResult, Exception> QueryStatementHandler::operator()
 
         if (statement.id)
         {
-            optimizedPlan.setQueryId(QueryId(*statement.id));
+            optimizedPlan.setQueryId(*statement.id);
         }
         const auto queryResult = queryManager->registerQuery(optimizedPlan);
         return queryResult

ESPAT-Orga/nebulastream-espat

nes-frontend/src/Statements/StatementHandler.cpp
@@ -234,7 +234,7 @@ std::expected<QueryStatementResult, Exception> QueryStatementHandler::operator()
 
         if (statement.id)
         {
-            plan.setQueryId(QueryId(*statement.id));
+            plan.setQueryId(*statement.id);
         }
         const auto queryResult = queryManager->registerQuery(plan);
         return queryResult
nebulastream/nebulastream

distributed systest configuration
Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

nes-systests/systest/include/SystestConfiguration.hpp
@@ -21,11 +21,18 @@
 #include <Configurations/BaseOption.hpp>
 #include <Configurations/ScalarOption.hpp>
 #include <Configurations/SequenceOption.hpp>
-#include <Util/URI.hpp>
 #include <SingleNodeWorkerConfiguration.hpp>
+#include <WorkerConfig.hpp>
 
 namespace NES
 {
+
+struct SystestClusterConfiguration
+{
+    std::vector<WorkerConfig> workers;
+    std::vector<HostAddr> allowSourcePlacement;
+    std::vector<HostAddr> allowSinkPlacement;
+};
 
 class SystestConfiguration final : public BaseConfiguration
 {
@@ -55,15 +62,11 @@ public:
     SequenceOption<StringOption> excludeGroups = {"exclude_groups", "test groups to exclude"};
     StringOption workerConfig = {"worker_config", "", "used worker config file (.yaml)"};
     StringOption queryCompilerConfig = {"query_compiler_config", "", "used query compiler config file (.yaml)"};
-    ScalarOption<NES::URI> grpcAddressUri
-        = {"grpc",
-           R"(The address to try to bind to the server in URI form. If
-the scheme name is omitted, "dns:///" is assumed. To bind to any address,
-please use IPv6 any, i.e., [::]:<port>, which also accepts IPv4
-connections.  Valid values include dns:///localhost:1234,
-192.168.1.1:31416, dns:///[::1]:27182, etc.)"};
+    BoolOption remoteWorker = {"remote_worker", "false", "use remote worker"};
+    StringOption clusterConfigPath = {"cluster_config", TEST_CONFIGURATION_DIR "/topologies/two-node.yaml", "cluster configuration"};
     BoolOption endlessMode = {"query_compiler_config", "false", "continuously issue queries to the worker"};
 
+    SystestClusterConfiguration clusterConfig;
     std::optional<SingleNodeWorkerConfiguration> singleNodeWorkerConfig;
 
 protected:

ESPAT-Orga/nebulastream-espat

nes-systests/systest/include/SystestConfiguration.hpp
@@ -21,12 +21,20 @@
 #include <Configurations/BaseOption.hpp>
 #include <Configurations/ScalarOption.hpp>
 #include <Configurations/SequenceOption.hpp>
-#include <Configurations/Validation/EndpointValidation.hpp>
+#include <Identifiers/Identifiers.hpp>
 #include <QueryOptimizerConfiguration.hpp>
 #include <SingleNodeWorkerConfiguration.hpp>
+#include <WorkerConfig.hpp>
 
 namespace NES
 {
+
+struct SystestClusterConfiguration
+{
+    std::vector<WorkerConfig> workers;
+    std::vector<Host> allowSourcePlacement;
+    std::vector<Host> allowSinkPlacement;
+};
 
 class SystestConfiguration final : public BaseConfiguration
 {
@@ -57,19 +65,8 @@ public:
     SequenceOption<StringOption> disabledTestFiles = {"disabled_test_files", "test files to disable"};
     StringOption workerConfig = {"worker_config", "", "used worker config file (.yaml)"};
     StringOption queryCompilerConfig = {"query_compiler_config", "", "used query compiler config file (.yaml)"};
-
-    /// Remote Tests: If enabled, systest will reach out to the gRPC address specified by grpcAddressUri
-    ScalarOption<bool> remoteTestExecution = {"remote_test_execution", "false", "run tests on remote worker"};
-    ScalarOption<std::string> grpcAddressUri
-        = {"grpc",
-           "[::]:8080",
-           R"(The address to try to bind to the server in URI form. If
-the scheme name is omitted, "dns:///" is assumed. To bind to any address,
-please use IPv6 any, i.e., [::]:<port>, which also accepts IPv4
-connections.  Valid values include dns:///localhost:1234,
-192.168.1.1:31416, dns:///[::1]:27182, etc.)",
-           {std::make_shared<EndpointValidation>(EndpointValidation::GRPC)}};
-
+    BoolOption remoteWorker = {"remote_worker", "false", "use remote worker"};
+    StringOption clusterConfigPath = {"cluster_config", TEST_CONFIGURATION_DIR "/topologies/two-node.yaml", "cluster configuration"};
     BoolOption showQueryPerformance = {"show_query_performance", "false", "print per-query performance timing in the console output"};
     BoolOption endlessMode = {"query_compiler_config", "false", "continuously issue queries to the worker"};
 
@@ -77,6 +74,7 @@ connections.  Valid values include dns:///localhost:1234,
     bool excludedGroupsProvidedOnCommandLine = false;
     std::vector<std::string> globalExcludedGroups;
 
+    SystestClusterConfiguration clusterConfig;
     std::optional<SingleNodeWorkerConfiguration> singleNodeWorkerConfig;
     std::optional<QueryOptimizerConfiguration> queryOptimizerConfig;
 
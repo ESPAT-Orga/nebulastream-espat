nebulastream/nebulastream

feat(Source/Sink): attach host configuration to source and sink descriptors
Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

nes-sources/tests/SourceCatalogTest.cpp
@@ -25,6 +25,7 @@
 #include <Configurations/Descriptor.hpp>
 #include <DataTypes/DataType.hpp>
 #include <DataTypes/DataTypeProvider.hpp>
+#include <Identifiers/Identifiers.hpp>
 #include <Identifiers/NESStrongType.hpp>
 #include <Sources/SourceCatalog.hpp>
 #include <Sources/SourceDescriptor.hpp>
@@ -73,8 +74,10 @@ TEST_F(SourceCatalogTest, AddRemovePhysicalSources)
 
     const auto sourceOpt = sourceCatalog.addLogicalSource("testSource", schema);
     ASSERT_TRUE(sourceOpt.has_value());
-    const auto physical1Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
-    const auto physical2Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical1Opt
+        = sourceCatalog.addPhysicalSource(*sourceOpt, "File", WorkerId("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical2Opt
+        = sourceCatalog.addPhysicalSource(*sourceOpt, "File", WorkerId("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
 
     ASSERT_TRUE(physical1Opt.has_value());
     ASSERT_TRUE(physical2Opt.has_value());
@@ -96,7 +99,8 @@ TEST_F(SourceCatalogTest, AddRemovePhysicalSources)
 
     ASSERT_TRUE(sourceCatalog.removePhysicalSource(physical1));
 
-    const auto physical3Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical3Opt
+        = sourceCatalog.addPhysicalSource(*sourceOpt, "File", WorkerId("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
     ASSERT_TRUE(physical2Opt.has_value());
     const auto& physical3 = physical3Opt.value();
 
@@ -119,7 +123,7 @@ TEST_F(SourceCatalogTest, AddInvalidPhysicalSource)
 
     const auto sourceOpt = sourceCatalog.addLogicalSource("testSource", schema);
     ASSERT_TRUE(sourceOpt.has_value());
-    const auto physical1Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "THIS_DOES_NOT_EXIST", {}, {});
+    const auto physical1Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "THIS_DOES_NOT_EXIST", WorkerId("localhost"), {}, {});
     ASSERT_FALSE(physical1Opt.has_value());
 }
 
@@ -133,8 +137,10 @@ TEST_F(SourceCatalogTest, RemoveLogicalSource)
     const auto sourceOpt = sourceCatalog.addLogicalSource("testSource", schema);
     ASSERT_TRUE(sourceOpt.has_value());
     const auto& logicalSource = sourceOpt.value();
-    const auto physical1Opt = sourceCatalog.addPhysicalSource(logicalSource, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
-    const auto physical2Opt = sourceCatalog.addPhysicalSource(logicalSource, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical1Opt
+        = sourceCatalog.addPhysicalSource(logicalSource, "File", WorkerId("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical2Opt
+        = sourceCatalog.addPhysicalSource(logicalSource, "File", WorkerId("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
 
     ASSERT_TRUE(physical1Opt.has_value());
     ASSERT_TRUE(physical2Opt.has_value());
@@ -195,8 +201,8 @@ TEST_F(SourceCatalogTest, ConcurrentSourceCatalogModification)
             }
             if (logicalSourceOpt.has_value())
             {
-                auto physicalSourceOpt
-                    = sourceCatalog.addPhysicalSource(*logicalSourceOpt, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+                auto physicalSourceOpt = sourceCatalog.addPhysicalSource(
+                    *logicalSourceOpt, "File", WorkerId("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
                 if (physicalSourceOpt.has_value())
                 {
                     successfulPhysicalAdds.fetch_add(1);

ESPAT-Orga/nebulastream-espat

nes-sources/tests/SourceCatalogTest.cpp
@@ -25,6 +25,7 @@
 #include <Configurations/Descriptor.hpp>
 #include <DataTypes/DataType.hpp>
 #include <DataTypes/DataTypeProvider.hpp>
+#include <Identifiers/Identifiers.hpp>
 #include <Identifiers/NESStrongType.hpp>
 #include <Sources/SourceCatalog.hpp>
 #include <Sources/SourceDescriptor.hpp>
@@ -73,8 +74,10 @@ TEST_F(SourceCatalogTest, AddRemovePhysicalSources)
 
     const auto sourceOpt = sourceCatalog.addLogicalSource("testSource", schema);
     ASSERT_TRUE(sourceOpt.has_value());
-    const auto physical1Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
-    const auto physical2Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical1Opt
+        = sourceCatalog.addPhysicalSource(*sourceOpt, "File", Host("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical2Opt
+        = sourceCatalog.addPhysicalSource(*sourceOpt, "File", Host("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
 
     ASSERT_TRUE(physical1Opt.has_value());
     ASSERT_TRUE(physical2Opt.has_value());
@@ -96,7 +99,8 @@ TEST_F(SourceCatalogTest, AddRemovePhysicalSources)
 
     ASSERT_TRUE(sourceCatalog.removePhysicalSource(physical1));
 
-    const auto physical3Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical3Opt
+        = sourceCatalog.addPhysicalSource(*sourceOpt, "File", Host("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
     ASSERT_TRUE(physical2Opt.has_value());
     const auto& physical3 = physical3Opt.value();
 
@@ -119,7 +123,7 @@ TEST_F(SourceCatalogTest, AddInvalidPhysicalSource)
 
     const auto sourceOpt = sourceCatalog.addLogicalSource("testSource", schema);
     ASSERT_TRUE(sourceOpt.has_value());
-    const auto physical1Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "THIS_DOES_NOT_EXIST", {}, {});
+    const auto physical1Opt = sourceCatalog.addPhysicalSource(*sourceOpt, "THIS_DOES_NOT_EXIST", Host("localhost"), {}, {});
     ASSERT_FALSE(physical1Opt.has_value());
 }
 
@@ -133,8 +137,10 @@ TEST_F(SourceCatalogTest, RemoveLogicalSource)
     const auto sourceOpt = sourceCatalog.addLogicalSource("testSource", schema);
     ASSERT_TRUE(sourceOpt.has_value());
     const auto& logicalSource = sourceOpt.value();
-    const auto physical1Opt = sourceCatalog.addPhysicalSource(logicalSource, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
-    const auto physical2Opt = sourceCatalog.addPhysicalSource(logicalSource, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical1Opt
+        = sourceCatalog.addPhysicalSource(logicalSource, "File", Host("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+    const auto physical2Opt
+        = sourceCatalog.addPhysicalSource(logicalSource, "File", Host("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
 
     ASSERT_TRUE(physical1Opt.has_value());
     ASSERT_TRUE(physical2Opt.has_value());
@@ -195,8 +201,8 @@ TEST_F(SourceCatalogTest, ConcurrentSourceCatalogModification)
             }
             if (logicalSourceOpt.has_value())
             {
-                auto physicalSourceOpt
-                    = sourceCatalog.addPhysicalSource(*logicalSourceOpt, "File", {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
+                auto physicalSourceOpt = sourceCatalog.addPhysicalSource(
+                    *logicalSourceOpt, "File", Host("localhost"), {{"file_path", "/dev/null"}}, {{"type", "CSV"}});
                 if (physicalSourceOpt.has_value())
                 {
                     successfulPhysicalAdds.fetch_add(1);
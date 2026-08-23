nebulastream/nebulastream

feat(QueryCompiler): adapt lowering to multi-sink logical plans

nes-query-compiler/tests/UnitTests/MultiRootLoweringTest.cpp
@@ -1,0 +1,189 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/// Multi-sink lowering into a shared physical DAG. Optimize a single-sink tree, graft a second sink onto
+/// the optimized plan (the deployment model: extra sinks attached after optimization), compile, and assert
+/// the sink/source counts. Without the fix apply() dropped all but the first sink, and the un-memoized
+/// lowerOperatorRecursively duplicated the shared subplan (source included) once per parent. Pipeline-internal
+/// structure of the shared part is a separate issue, not asserted here.
+/// The post-optimizer graft copies the second sink's TraitSet from the first (the per-rule optimizer does
+/// not yet preserve multi-root sharing, #1753), so these tests pin a trait config no real plan has. Drop
+/// the copy once #1753 lands.
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <Util/Logger/LogLevel.hpp>
+#include <Util/Logger/impl/NesLogger.hpp>
+#include <gtest/gtest.h>
+#include <BaseUnitTest.hpp>
+
+#include <DataTypes/DataType.hpp>
+#include <DataTypes/DataTypeProvider.hpp>
+#include <DataTypes/Schema.hpp>
+#include <DataTypes/SchemaFwd.hpp>
+#include <DataTypes/UnboundField.hpp>
+#include <Functions/ComparisonFunctions/GreaterEqualsLogicalFunction.hpp>
+#include <Functions/ConstantValueLogicalFunction.hpp>
+#include <Functions/FieldAccessLogicalFunction.hpp>
+#include <Identifiers/Identifier.hpp>
+#include <Identifiers/Identifiers.hpp>
+#include <Operators/LogicalOperator.hpp>
+#include <Operators/SelectionLogicalOperator.hpp>
+#include <Operators/Sinks/SinkLogicalOperator.hpp>
+#include <Operators/Sources/SourceDescriptorLogicalOperator.hpp>
+#include <Phases/RuleBasedOptimizer.hpp>
+#include <Plans/LogicalPlan.hpp>
+#include <Sinks/SinkCatalog.hpp>
+#include <Sinks/SinkDescriptor.hpp>
+#include <Sources/SourceCatalog.hpp>
+#include <Sources/SourceDescriptor.hpp>
+#include <CompiledQueryPlan.hpp>
+#include <ErrorHandling.hpp>
+#include <ModelCatalog.hpp>
+#include <QueryCompiler.hpp>
+#include <QueryExecutionConfiguration.hpp>
+#include <QueryId.hpp>
+#include <QueryOptimizerConfiguration.hpp>
+
+namespace NES
+{
+/// NOLINTBEGIN(bugprone-unchecked-optional-access)
+namespace
+{
+
+class MultiRootLoweringTest : public Testing::BaseUnitTest
+{
+public:
+    static void SetUpTestSuite() { Logger::setupLogging("MultiRootLoweringTest.log", LogLevel::LOG_DEBUG); }
+
+    static Schema<UnqualifiedUnboundField, Ordered> createSchema()
+    {
+        std::vector<UnqualifiedUnboundField> fields;
+        fields.emplace_back(Identifier::parse("id"), DataTypeProvider::provideDataType(DataType::Type::UINT64));
+        fields.emplace_back(Identifier::parse("value"), DataTypeProvider::provideDataType(DataType::Type::UINT64));
+        return Schema<UnqualifiedUnboundField, Ordered>{fields};
+    }
+
+    TypedLogicalOperator<SourceDescriptorLogicalOperator> createSource(const std::string& name)
+    {
+        const auto logicalSource = sourceCatalog->addLogicalSource(Identifier::parse(name), createSchema());
+        EXPECT_TRUE(logicalSource.has_value());
+        const std::unordered_map<Identifier, std::string> sourceConfig{{Identifier::parse("FILE_PATH"), "/dev/null"}};
+        const std::unordered_map<Identifier, std::string> parserConfig{{Identifier::parse("TYPE"), "CSV"}};
+        auto descriptor = sourceCatalog->addPhysicalSource(
+            logicalSource.value(), Identifier::parse("file"), Host{"localhost"}, sourceConfig, parserConfig);
+        EXPECT_TRUE(descriptor.has_value());
+        return SourceDescriptorLogicalOperator::create(std::move(descriptor.value()));
+    }
+
+    SinkDescriptor createSinkDescriptor(const std::string& name)
+    {
+        const std::unordered_map<Identifier, std::string> sinkConfig{
+            {Identifier::parse("FILE_PATH"), "/dev/null"}, {Identifier::parse("OUTPUT_FORMAT"), "CSV"}};
+        auto descriptor = sinkCatalog->addSinkDescriptor(
+            Identifier::parse(name), createSchema(), Identifier::parse("file"), Host{"localhost"}, sinkConfig, {});
+        EXPECT_TRUE(descriptor.has_value());
+        return descriptor.value();
+    }
+
+    /// Builds source -> selection -> sink and runs it through the real optimizer to obtain a fully
+    /// bound (traits attached) single-root plan.
+    LogicalPlan makeOptimizedSingleSinkPlan(const std::string& sourceName, const std::string& sinkName)
+    {
+        const auto source = createSource(sourceName);
+        const auto predicate = GreaterEqualsLogicalFunction{
+            FieldAccessLogicalFunction{source.getOutputSchema()[Identifier::parse("id")].value()},
+            ConstantValueLogicalFunction{DataType{DataType::Type::UINT64, DataType::NULLABLE::NOT_NULLABLE}, "0"}};
+        const auto selection = SelectionLogicalOperator::create(source, predicate);
+        const auto sink = SinkLogicalOperator::create(selection, createSinkDescriptor(sinkName));
+        auto plan
+            = LogicalPlan{QueryId::create(LocalQueryId{LocalQueryId::INVALID}, DistributedQueryId{DistributedQueryId::INVALID}), {sink}};
+        return RuleBasedOptimizer{QueryOptimizerConfiguration{}, sourceCatalog, sinkCatalog, modelCatalog}.optimize(std::move(plan));
+    }
+
+    static LogicalOperator leafOf(LogicalOperator op)
+    {
+        while (!op.getChildren().empty())
+        {
+            op = op.getChildren()[0];
+        }
+        return op;
+    }
+
+    static std::unique_ptr<CompiledQueryPlan> compile(LogicalPlan plan)
+    {
+        auto compiler = QueryCompilation::QueryCompiler{QueryExecutionConfiguration{}};
+        return compiler.compileQuery(std::make_unique<QueryCompilation::QueryCompilationRequest>(
+            QueryCompilation::QueryCompilationRequest{.queryPlan = std::move(plan)}));
+    }
+
+    std::shared_ptr<SourceCatalog> sourceCatalog = std::make_shared<SourceCatalog>();
+    std::shared_ptr<SinkCatalog> sinkCatalog = std::make_shared<SinkCatalog>();
+    std::shared_ptr<ModelCatalog> modelCatalog = std::make_shared<ModelCatalog>();
+};
+
+/// Two sink roots sharing one optimized subplan: the compiled plan must contain BOTH sinks (and one
+/// source, i.e. the shared subplan lowered exactly once).
+TEST_F(MultiRootLoweringTest, SecondSinkRootIsNotDropped)
+{
+    const auto optimized = makeOptimizedSingleSinkPlan("multiroot", "multiroot_sink1");
+    ASSERT_EQ(optimized.getRootOperators().size(), 1U);
+    const auto firstSink = optimized.getRootOperators()[0];
+    ASSERT_EQ(firstSink.getChildren().size(), 1U);
+
+    /// Graft a second sink sharing the first sink's child; the sink-level traits are copied from the
+    /// optimizer-bound first sink (see file header), everything below is shared as-is.
+    const auto secondSink
+        = LogicalOperator{SinkLogicalOperator::create(firstSink.getChildren()[0], createSinkDescriptor("multiroot_sink2"))}.withTraitSet(
+            firstSink.getTraitSet());
+    auto plan = LogicalPlan{optimized.getQueryId(), {firstSink, secondSink}};
+
+    const auto compiled = compile(std::move(plan));
+
+    ASSERT_TRUE(compiled);
+    EXPECT_EQ(compiled->sinks.size(), 2U) << "the second sink root was silently dropped by LowerToPhysicalOperators::apply";
+    EXPECT_EQ(compiled->sources.size(), 1U) << "the shared subplan (incl. the source) was lowered once per root";
+}
+
+/// Two sink roots sharing the SAME source instance (second sink grafted directly onto the source leaf):
+/// the shared source must be lowered exactly once — otherwise the input is physically read twice.
+TEST_F(MultiRootLoweringTest, SharedSourceIsLoweredOnce)
+{
+    const auto optimized = makeOptimizedSingleSinkPlan("sharedsource", "sharedsource_sink1");
+    ASSERT_EQ(optimized.getRootOperators().size(), 1U);
+    const auto firstSink = optimized.getRootOperators()[0];
+    const auto source = leafOf(firstSink);
+    ASSERT_TRUE(source.tryGetAs<SourceDescriptorLogicalOperator>().has_value());
+
+    /// Same trait-copy scaffolding as above (see file header).
+    const auto secondSink = LogicalOperator{SinkLogicalOperator::create(source, createSinkDescriptor("sharedsource_sink2"))}.withTraitSet(
+        firstSink.getTraitSet());
+    auto plan = LogicalPlan{optimized.getQueryId(), {firstSink, secondSink}};
+
+    const auto compiled = compile(std::move(plan));
+
+    ASSERT_TRUE(compiled);
+    EXPECT_EQ(compiled->sinks.size(), 2U) << "the second sink root was silently dropped by LowerToPhysicalOperators::apply";
+    EXPECT_EQ(compiled->sources.size(), 1U)
+        << "the source shared by both sink roots was lowered once per path — the input would be read twice";
+}
+
+}
+
+/// NOLINTEND(bugprone-unchecked-optional-access)
+}

ESPAT-Orga/nebulastream-espat

nes-query-compiler/tests/UnitTests/MultiRootLoweringTest.cpp
@@ -1,0 +1,189 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/// Multi-sink lowering into a shared physical DAG. Optimize a single-sink tree, graft a second sink onto
+/// the optimized plan (the deployment model: extra sinks attached after optimization), compile, and assert
+/// the sink/source counts. Without the fix apply() dropped all but the first sink, and the un-memoized
+/// lowerOperatorRecursively duplicated the shared subplan (source included) once per parent. Pipeline-internal
+/// structure of the shared part is a separate issue, not asserted here.
+/// The post-optimizer graft copies the second sink's TraitSet from the first (the per-rule optimizer does
+/// not yet preserve multi-root sharing, #1753), so these tests pin a trait config no real plan has. Drop
+/// the copy once #1753 lands.
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <Util/Logger/LogLevel.hpp>
+#include <Util/Logger/impl/NesLogger.hpp>
+#include <gtest/gtest.h>
+#include <BaseUnitTest.hpp>
+
+#include <DataTypes/DataType.hpp>
+#include <DataTypes/DataTypeProvider.hpp>
+#include <DataTypes/UnboundField.hpp>
+#include <Functions/ComparisonFunctions/GreaterEqualsLogicalFunction.hpp>
+#include <Functions/ConstantValueLogicalFunction.hpp>
+#include <Functions/FieldAccessLogicalFunction.hpp>
+#include <Identifiers/Identifier.hpp>
+#include <Identifiers/Identifiers.hpp>
+#include <Operators/LogicalOperator.hpp>
+#include <Operators/SelectionLogicalOperator.hpp>
+#include <Operators/Sinks/SinkLogicalOperator.hpp>
+#include <Operators/Sources/SourceDescriptorLogicalOperator.hpp>
+#include <Phases/RuleBasedOptimizer.hpp>
+#include <Plans/LogicalPlan.hpp>
+#include <Schema/Schema.hpp>
+#include <Schema/SchemaFwd.hpp>
+#include <Sinks/SinkCatalog.hpp>
+#include <Sinks/SinkDescriptor.hpp>
+#include <Sources/SourceCatalog.hpp>
+#include <Sources/SourceDescriptor.hpp>
+#include <CompiledQueryPlan.hpp>
+#include <ErrorHandling.hpp>
+#include <ModelCatalog.hpp>
+#include <QueryCompiler.hpp>
+#include <QueryExecutionConfiguration.hpp>
+#include <QueryId.hpp>
+#include <QueryOptimizerConfiguration.hpp>
+
+namespace NES
+{
+/// NOLINTBEGIN(bugprone-unchecked-optional-access)
+namespace
+{
+
+class MultiRootLoweringTest : public Testing::BaseUnitTest
+{
+public:
+    static void SetUpTestSuite() { Logger::setupLogging("MultiRootLoweringTest.log", LogLevel::LOG_DEBUG); }
+
+    static Schema<UnqualifiedUnboundField, Ordered> createSchema()
+    {
+        std::vector<UnqualifiedUnboundField> fields;
+        fields.emplace_back(Identifier::parse("id"), DataTypeProvider::provideDataType(DataType::Type::UINT64));
+        fields.emplace_back(Identifier::parse("value"), DataTypeProvider::provideDataType(DataType::Type::UINT64));
+        return Schema<UnqualifiedUnboundField, Ordered>{fields};
+    }
+
+    TypedLogicalOperator<SourceDescriptorLogicalOperator> createSource(const std::string& name)
+    {
+        const auto logicalSource = sourceCatalog->addLogicalSource(Identifier::parse(name), createSchema());
+        EXPECT_TRUE(logicalSource.has_value());
+        const std::unordered_map<Identifier, std::string> sourceConfig{{Identifier::parse("FILE_PATH"), "/dev/null"}};
+        const std::unordered_map<Identifier, std::string> parserConfig{{Identifier::parse("TYPE"), "CSV"}};
+        auto descriptor = sourceCatalog->addPhysicalSource(
+            logicalSource.value(), Identifier::parse("file"), Host{"localhost"}, sourceConfig, parserConfig);
+        EXPECT_TRUE(descriptor.has_value());
+        return SourceDescriptorLogicalOperator::create(std::move(descriptor.value()));
+    }
+
+    SinkDescriptor createSinkDescriptor(const std::string& name)
+    {
+        const std::unordered_map<Identifier, std::string> sinkConfig{
+            {Identifier::parse("FILE_PATH"), "/dev/null"}, {Identifier::parse("OUTPUT_FORMAT"), "CSV"}};
+        auto descriptor = sinkCatalog->addSinkDescriptor(
+            Identifier::parse(name), createSchema(), Identifier::parse("file"), Host{"localhost"}, sinkConfig, {});
+        EXPECT_TRUE(descriptor.has_value());
+        return descriptor.value();
+    }
+
+    /// Builds source -> selection -> sink and runs it through the real optimizer to obtain a fully
+    /// bound (traits attached) single-root plan.
+    LogicalPlan makeOptimizedSingleSinkPlan(const std::string& sourceName, const std::string& sinkName)
+    {
+        const auto source = createSource(sourceName);
+        const auto predicate = GreaterEqualsLogicalFunction{
+            FieldAccessLogicalFunction{source.getOutputSchema()[Identifier::parse("id")].value()},
+            ConstantValueLogicalFunction{DataType{DataType::Type::UINT64, DataType::NULLABLE::NOT_NULLABLE}, "0"}};
+        const auto selection = SelectionLogicalOperator::create(source, predicate);
+        const auto sink = SinkLogicalOperator::create(selection, createSinkDescriptor(sinkName));
+        auto plan
+            = LogicalPlan{QueryId::create(LocalQueryId{LocalQueryId::INVALID}, DistributedQueryId{DistributedQueryId::INVALID}), {sink}};
+        return RuleBasedOptimizer{QueryOptimizerConfiguration{}, sourceCatalog, sinkCatalog, modelCatalog}.optimize(std::move(plan));
+    }
+
+    static LogicalOperator leafOf(LogicalOperator op)
+    {
+        while (!op.getChildren().empty())
+        {
+            op = op.getChildren()[0];
+        }
+        return op;
+    }
+
+    static std::unique_ptr<CompiledQueryPlan> compile(LogicalPlan plan)
+    {
+        auto compiler = QueryCompilation::QueryCompiler{QueryExecutionConfiguration{}};
+        return compiler.compileQuery(std::make_unique<QueryCompilation::QueryCompilationRequest>(
+            QueryCompilation::QueryCompilationRequest{.queryPlan = std::move(plan)}));
+    }
+
+    std::shared_ptr<SourceCatalog> sourceCatalog = std::make_shared<SourceCatalog>();
+    std::shared_ptr<SinkCatalog> sinkCatalog = std::make_shared<SinkCatalog>();
+    std::shared_ptr<ModelCatalog> modelCatalog = std::make_shared<ModelCatalog>();
+};
+
+/// Two sink roots sharing one optimized subplan: the compiled plan must contain BOTH sinks (and one
+/// source, i.e. the shared subplan lowered exactly once).
+TEST_F(MultiRootLoweringTest, SecondSinkRootIsNotDropped)
+{
+    const auto optimized = makeOptimizedSingleSinkPlan("multiroot", "multiroot_sink1");
+    ASSERT_EQ(optimized.getRootOperators().size(), 1U);
+    const auto firstSink = optimized.getRootOperators()[0];
+    ASSERT_EQ(firstSink.getChildren().size(), 1U);
+
+    /// Graft a second sink sharing the first sink's child; the sink-level traits are copied from the
+    /// optimizer-bound first sink (see file header), everything below is shared as-is.
+    const auto secondSink
+        = LogicalOperator{SinkLogicalOperator::create(firstSink.getChildren()[0], createSinkDescriptor("multiroot_sink2"))}.withTraitSet(
+            firstSink.getTraitSet());
+    auto plan = LogicalPlan{optimized.getQueryId(), {firstSink, secondSink}};
+
+    const auto compiled = compile(std::move(plan));
+
+    ASSERT_TRUE(compiled);
+    EXPECT_EQ(compiled->sinks.size(), 2U) << "the second sink root was silently dropped by LowerToPhysicalOperators::apply";
+    EXPECT_EQ(compiled->sources.size(), 1U) << "the shared subplan (incl. the source) was lowered once per root";
+}
+
+/// Two sink roots sharing the SAME source instance (second sink grafted directly onto the source leaf):
+/// the shared source must be lowered exactly once — otherwise the input is physically read twice.
+TEST_F(MultiRootLoweringTest, SharedSourceIsLoweredOnce)
+{
+    const auto optimized = makeOptimizedSingleSinkPlan("sharedsource", "sharedsource_sink1");
+    ASSERT_EQ(optimized.getRootOperators().size(), 1U);
+    const auto firstSink = optimized.getRootOperators()[0];
+    const auto source = leafOf(firstSink);
+    ASSERT_TRUE(source.tryGetAs<SourceDescriptorLogicalOperator>().has_value());
+
+    /// Same trait-copy scaffolding as above (see file header).
+    const auto secondSink = LogicalOperator{SinkLogicalOperator::create(source, createSinkDescriptor("sharedsource_sink2"))}.withTraitSet(
+        firstSink.getTraitSet());
+    auto plan = LogicalPlan{optimized.getQueryId(), {firstSink, secondSink}};
+
+    const auto compiled = compile(std::move(plan));
+
+    ASSERT_TRUE(compiled);
+    EXPECT_EQ(compiled->sinks.size(), 2U) << "the second sink root was silently dropped by LowerToPhysicalOperators::apply";
+    EXPECT_EQ(compiled->sources.size(), 1U)
+        << "the source shared by both sink roots was lowered once per path — the input would be read twice";
+}
+
+}
+
+/// NOLINTEND(bugprone-unchecked-optional-access)
+}